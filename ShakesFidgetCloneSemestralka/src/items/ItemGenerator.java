38b54c6cf4e5b7ac1f8f280605d8cf99a8a756a1
snap22 <39875321+snap22@users.noreply.github.com>
2020-03-21 20:06:57 +0100

    Added itemGenerator
    provizorny item generator, zatial len pridane metody a zoznam stringov pre potencialne mena

--- ShakesFidgetCloneSemestralka/src/items/ItemGenerator.java
+++ ShakesFidgetCloneSemestralka/src/items/ItemGenerator.java
@@ -5,9 +5,45 @@
  */
 package items;
 
+import items.equippable.Armor;
+import items.equippable.Equipment;
+import items.equippable.Helmet;
+import items.equippable.Weapon;
+import items.valuables.Goods;
+import java.util.Random;
+
 /**
  * Generuje itemy..
  */
-public class ItemGenerator {
+public abstract class ItemGenerator {
+    private String[] adjectives = {"Iron", "Golden", "Wooden", "Elemental", "Magical", "Overpowered", "Majestic", "Astounishing", 
+        "Stone", "Brutal", "Bloody", "Savage", "Empowered", "Practical", "Diamon", "Gladiator", "Training"};
+    private String[] weaponNames = {"Sword", "Cleaver", "Axe", "Machette", "Mace", "Hammer"};
+    private Random random  = new Random();
     
+    public static Weapon generateWeapon() {
+        
+        return null;
+    }
+    
+    public static Helmet generateHelmet() {
+        return null;
+    }
+    
+    public static Armor generateArmor() {
+        return null;
+    }
+    
+    public static Goods generateGoods() {
+        return null;
+    }
+    
+    public static Equipment generateEquipment() {
+        return null;
+    }
+    
+    
+    public static Item2 generateItem() {
+        return null;
+    }
 }
